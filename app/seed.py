df6dd191caa0055bc329d71ff1de5a70b9113fba
tray <onesimus.githua@student.moringaschool.com>
2023-12-06 15:30:51 +0300

    completed assignment

---
+++ app/seed.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from faker import Faker
+import random
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Game
+
+fake = Faker()
+
+if __name__ == '__main__':
+    
+    engine = create_engine('sqlite:///seed_db.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(Game).delete()
+    session.commit()
+
+...
+
+print("Seeding games...")
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+for i in range(50)]
+
+session.bulk_save_objects(games)
+session.commit()
